02e2495363a0a28be9c80e824fd4e6b78b1cb4b1
mdctenga <mdctenga@gmail.com>
2015-07-29 21:06:45 -1000

    Add multiple items to database

--- models/product.js
+++ models/product.js
@@ -3,7 +3,7 @@ module.exports = function(sequelize, DataTypes) {
   var Product = sequelize.define('Product', {
     name: DataTypes.TEXT,
     description: DataTypes.TEXT,
-    price: DataTypes.DECIMAL(2)
+    price: DataTypes.DECIMAL(10,2)
   }, {
     classMethods: {
       associate: function(models) {
--- seed/products.js
+++ seed/products.js
@@ -5,10 +5,15 @@ var faker = require('faker');
 models.sequelize
   .sync({ force: true })
   .then(function () {
-    Product.bulkCreate({
-      name: "Matt",
-      description: "Tenga",
-      price: 10
-    });
+    var productArray = [];
+    for (var i = 0; i < 10; i += 1) {
+      productArray.push({
+        name: faker.commerce.productName(),
+        description: faker.commerce.productAdjective() + " " + faker.commerce.productMaterial() ,
+        price: parseFloat(faker.commerce.price())
+      });
+    }
+
+    Product.bulkCreate(productArray);
     console.log("connected to the database!");
   });
