391deaafb2f9892f296155598f77d675fb5d7b6f
mdctenga <mdctenga@gmail.com>
2015-08-01 13:50:28 -1000

    Implement product routes

--- server.js
+++ server.js
@@ -12,20 +12,44 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.get('/products', function (req, res, next) {
-  res.send(req.params);
+server.get('/products', function (req, res) {
+  models.Product.findAll()
+    .then(function (products) {
+      return res.json(products);
+    });
 });
 
-server.get('/products/:id', function (req, res, next) {
-  res.send(req.params);
+server.get('/products/:id', function (req, res) {
+  models.Product.findById(req.params.id)
+    .then(function (product) {
+      if(product) {
+        return res.json(product);
+      } else {
+        res.json({});
+      }
+    });
 });
 
 server.get('/orders', function (req, res, next) {
-  res.send(req.params);
+  models.Product.findAll()
+    .then(function (products) {
+      return res.json(products);
+    });
 });
 
+// server.post('/orders', function (req, res, next) {
+//   res.send(req.params);
+// });
+
 server.get('/orders/:id', function (req, res, next) {
-  res.send(req.params);
+  models.Product.findById(req.params.id)
+    .then(function (product) {
+      if(product) {
+        return res.json(product);
+      } else {
+        res.json({});
+      }
+    });
 });
 
 server.listen(config.port, function () {
